4682df7b4c4aeacb36bbc727883a821e7a5ab0b2
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-11-21 00:38:22 +0700

    menambahkan resources serta variabel email dan no hp

--- app/src/main/java/com/example/arsitekturmvvm/model/Mahasiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/model/Mahasiswa.kt
@@ -1,7 +1,11 @@
 package com.example.arsitekturmvvm.model
 
+import android.provider.ContactsContract.CommonDataKinds.Email
+
 data class Mahasiswa(
     val nama: String = "",
     val gender: String = "",
+    val email: String = "",
+    val NoHP: String = "",
     val alamat: String = ""
 )
