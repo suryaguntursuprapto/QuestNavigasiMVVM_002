320835508566b1e7e6da2e60f7dc1e114775663b
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-11-20 23:19:46 +0700

    setting destinasi halaman pertama pada navhost

--- app/src/main/java/com/example/arsitekturmvvm/navigasi.kt
+++ app/src/main/java/com/example/arsitekturmvvm/navigasi.kt
@@ -1,9 +1,14 @@
 package com.example.arsitekturmvvm
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.arsitekturmvvm.ui.viewmodel.MahasiswaViewModel
 
@@ -17,6 +22,14 @@ fun Navigasi(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
 ){
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ){
 
+        }
+    }
 }
 
